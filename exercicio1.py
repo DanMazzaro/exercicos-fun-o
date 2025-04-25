84e571fdba73a2dad2bcc1276565752c20d4d23c
DanMazzaro <danielmazzaro44@gmail.com>
2025-04-25 10:21:07 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,11 @@
+# Faça um código que receba o nome de uma pessoa. Mostre o nome
+# utilizando função e também mostre a mensagem “Te desejo tudo de bom.”
+# Utilizando função.
+
+
+nome =  input('digite o nome da pessoa')
+
+def mensagem(nome):
+    print(f'{nome},Te desejo tudo de bom')
+
+men = mensagem(nome)
---
+++ exercicio2.py
@@ -0,0 +1,28 @@
+# Faça um código que receba dois números inteiros, utilize as quatro
+# operações básicas pra gerar resultados com esses dois números e mostre os
+# resultados. Cada operação deve utilizar uma função.
+
+num1 = int(input('digite um numero inteiro: '))
+num2 = int(input('digite um numero inteiro: '))
+opcao = input('escolha a operação: ')
+
+while True:
+
+def soma(num1,num2):
+    calculo = num1 + num2
+
+
+
+def subtracao(num1,num2):
+    return num1 - num2
+
+
+def mutiplicacao(num1,num2):
+    return num1 * num2
+
+
+def divisao(num1,num2):
+    return num1 / num2
+
+
+resultado = soma()
